ee311e3293835cdd98990b485d9e0a0b3b090e82
Connos91 <con.onisifo@gmail.com>
2022-03-26 01:06:11 +0200

    add RxJS library for searching functionality

--- components/Seach/Seach.js
+++ components/Seach/Seach.js
@@ -1,14 +1,41 @@
+import React, { useEffect, useState } from "react";
+
 import { useDispatch } from "react-redux";
 import { searchCharacter } from "../../redux/actions/characterActions";
-import { getCharacters } from "../../redux/actions/characterActions";
 import styles from "../../styles/Home.module.css";
 import Logo from "../../assets/images/Logo..svg";
 import Image from "next/image";
 import Link from "next/link";
+import {
+  BehaviorSubject,
+  debounceTime,
+  distinctUntilChanged,
+  filter,
+  from,
+  mergeMap,
+} from "rxjs";
 
 const Search = (props) => {
   const dispatch = useDispatch();
   const classes = `form-control ${styles.search}`;
+
+  const useObservable = (observable) => {
+    useEffect(() => {
+      let subscription = observable.subscribe();
+
+      return () => subscription.unsubscribe();
+    }, [observable]);
+  };
+
+  let searcSubject = new BehaviorSubject("");
+  let searchResultOnservable = searcSubject.pipe(
+    filter((val) => val.length > 1),
+    debounceTime(750), //weonly wants to execute this after user stops typing after 750 milliseconds
+    distinctUntilChanged(), // Only emit when the current value is different than the last -- we dont want to execute the same call again
+    mergeMap((val) => from(dispatch(searchCharacter(val))))
+  );
+
+  useObservable(searchResultOnservable);
 
   const searchHero = (e) => {
     if (e.target.value === "") {
@@ -18,9 +45,8 @@ const Search = (props) => {
         functionType: "refresh",
       });
     } else {
-      dispatch(searchCharacter(e.target.value));
+      searcSubject.next(e.target.value);
     }
-    console.log(e.target.value);
   };
 
   return (
@@ -33,6 +59,7 @@ const Search = (props) => {
         <input
           type="text"
           id="search"
+          autoComplete="off"
           className={classes}
           name="serch"
           placeholder="Search Hero"
